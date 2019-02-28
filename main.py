2664629af51a63d935834f2b77b1ada6ee1443fb
reritom <reikudjinn@gmail.com>
2019-02-28 22:20:15 +0100

    Surroundings in perimeter fixed

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
-N = 20
+N = 100
 maze = Maze(N, N)
-surroundings = maze.get_surroundings((int(N/2), int(N/2)), 5)
-print(surroundings)
+maze.add_block((int(N/2)+5, int(N/2)+5))
+surroundings = maze.get_surroundings((int(N/2), int(N/2)), 30)
+#print(surroundings)
+print(maze.blocks)
 
 
 # make an empty data set
@@ -25,8 +27,8 @@ my_cmap = matplotlib.colors.ListedColormap(['r', 'g', 'b'])
 my_cmap.set_bad(color='w', alpha=0)
 # draw the grid
 for x in range(N + 1):
-    ax.axhline(x, lw=2, color='k', zorder=5)
-    ax.axvline(x, lw=2, color='k', zorder=5)
+    ax.axhline(x, lw=0, color='k', zorder=5)
+    ax.axvline(x, lw=0, color='k', zorder=5)
 # draw the boxes
 ax.imshow(data, interpolation='none', cmap=my_cmap, extent=[0, N, 0, N], zorder=0)
 # turn off the axis labels
--- maze.py
+++ maze.py
@@ -11,14 +11,50 @@ class Maze:
         if view_range == 0:
             return set(position)
 
+        """
         surroundings = set()
         for radius in range(view_range):
             surroundings = surroundings.union(self.bresenhams_circle(centre_x, centre_y, radius))
 
         return surroundings
+        """
+        perimeter = self.bresenhams_circle(centre_x, centre_y, view_range)
 
-    def add_block(self, x, y):
-        self.blocks = self.blocks.union((x, y))
+        # We will fill the area within the perimeter
+        surroundings = set().union(perimeter)
+
+        for x in range(centre_x - view_range, centre_x + view_range):
+            # Find any perimeter points for this x value
+            perimeter_points = list(filter(lambda this_point: this_point[0] == x, perimeter))
+
+            if len(perimeter_points) == 1:
+                pass
+            elif len(perimeter_points) > 1:
+                # We will fill all the points between the two/four perimeter points
+                max_y = max([point[1] for point in perimeter_points])
+                min_y = min([point[1] for point in perimeter_points])
+
+                for y in range(min_y, max_y):
+                    surroundings.add((x, y))
+            else:
+                pass
+
+        # Look for any blocks within our surroundings
+        relevent_blocks = []
+        for block in self.blocks:
+            if block in surroundings:
+                relevent_blocks.append(block)
+
+        # For each relevent block, determine which of the surroundings it shadows, and those that aren't fully clear
+        for block in relevent_blocks:
+            pass
+
+        # Strip any that are outside of the grid
+
+        return surroundings
+
+    def add_block(self, block: tuple):
+        self.blocks.add(block)
 
     def remove_block(self, x, y):
         pass
